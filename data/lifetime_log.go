bd521d95240931f0536aa8ecf5abe77e207f65c4
RohitBhandarkar <rohitbhandarkar@hotmail.com>
2025-07-21 23:08:26 +0530

    feat: process monitoring

--- data/lifetime_log.go
+++ data/lifetime_log.go
@@ -1,73 +1,110 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"time"
 	"github.com/shirou/gopsutil/v4/process"
+	"encoding/json"
+	"os"
 )
 
-func main() {
-	fmt.Println("hello")
-	processIDs, err := process.Processes()
-	if err != nil {
-		fmt.Println("Error getting processes:", err)
-		return
-	}
-	fmt.Println("Processes:", len(processIDs))
+type ProcessInfo struct {
+	Pid        int32
+	Pname      string
+	Timestamp  time.Time
+	CpuTimes   map[string]float64
+	Lifetime   int64
+	CpuUsage   float64
+	IoCounters map[string]uint64
+	Status     string
+}
 
-	for pid := range processIDs { //proc is the process ID
-		proc, _ := process.NewProcess(int32(pid))
-		if err != nil {
-			fmt.Println("Error creating process:", err)
-			continue
-		}
+func main() {
+	log.Println("Starting process info logger...")
+
+	processes, err := process.Processes()
+	if err != nil {
+		log.Fatalf("Error getting processes: %v", err)
+	}
+	log.Printf("Found %d processes", len(processes))
+
+	data := make([]ProcessInfo, 0, len(processes))
+
+	for _, proc := range processes {
 		now := time.Now()
-		println("Process ID:", pid, "Timestamp:", now.Unix())
-		
+		errors := false
+		pid := proc.Pid
 		name, err := proc.Name()
-    	if err != nil {
-        	fmt.Println("Error getting process name:", err)
-        	name = "unknown"
-    	} else{
-    		fmt.Printf("Process ID: %d, Name: %s, Timestamp: %d\n", pid, name, now.Unix())
+		if err != nil && !errors {
+			log.Printf("PID %d: Error getting process name: %v", pid, err)
+			name = "unknown"
+			errors = true
 		}
 
 		createTime, err := proc.CreateTime()
+		var lifetimeMs int64
 		if err != nil {
-			fmt.Println("Error getting create time:", err)
+			log.Printf("PID %d: Error getting create time: %v", pid, err)
+			errors = true
 		} else {
-			lifetime := now.UnixMilli() - createTime
-			fmt.Printf("  Lifetime (ms): %d\n", lifetime)
+			lifetimeMs = now.UnixMilli() - createTime
 		}
 
 		cpuPercent, err := proc.CPUPercent()
-		if err != nil {
-			fmt.Println("Error getting CPU percent:", err)
-		} else {
-			fmt.Printf("  CPU Usage (%%): %.2f\n", cpuPercent)
+		if err != nil{
+			log.Printf("PID %d: Error getting CPU percent: %v", pid, err)
+			errors = true
 		}
 
-		cpuTimes, err := proc.Times()
+		cpuTimesObj, err := proc.Times()
 		if err != nil {
-			fmt.Println("Error getting CPU times:", err)
-		} else {
-			fmt.Printf("  CPU Times: %+v\n", cpuTimes)
+			log.Printf("PID %d: Error getting CPU times: %v", pid, err)
+			errors = true
 		}
 
-		ioCounters, err := proc.IOCounters()
+		ioCountersObj, err := proc.IOCounters()
 		if err != nil {
-			fmt.Println("Error getting IO counters:", err)
-		} else {
-			fmt.Printf("  IO Counters: %+v\n", ioCounters)
+			log.Printf("PID %d: Error getting IO counters: %v", pid, err)
+			errors = true
 		}
 
-		status, err := proc.Status()
+		statusStr, err := proc.Status()
 		if err != nil {
-			fmt.Println("Error getting status:", err)
-		} else {
-			fmt.Printf("  Status: %s\n", status)
+			log.Printf("PID %d: Error getting status: %v", pid, err)
+			errors = true
 		}
-		
+
+		log.Printf(
+			"PID: %d | Name: %s | Timestamp: %d | Lifetime(ms): %d | CPU Usage(%%): %.2f | CPU Times: %+v | IO Counters: %+v | Status: %s",
+			pid, name, now.Unix(), lifetimeMs, cpuPercent, cpuTimesObj, ioCountersObj, statusStr,
+		)
+
+		if !errors {
+			processInfo := ProcessInfo{
+				Pid:        pid,
+				Pname:      name,
+				Timestamp:  now,
+				CpuTimes:   map[string]float64{"user": cpuTimesObj.User, "system": cpuTimesObj.System},
+				Lifetime:   lifetimeMs,
+				CpuUsage:   cpuPercent,
+				IoCounters: map[string]uint64{"read_count": ioCountersObj.ReadCount, "write_count": ioCountersObj.WriteCount},
+				Status:     statusStr[0],
+			}
+			data = append(data, processInfo)
+			log.Printf("Process Info added")
+		}
 	}
+	outputFile, err := os.Create("../artifacts/process_data.json")
+	if err != nil {
+		log.Fatalf("Error creating JSON file: %v", err)
+	}
+	defer outputFile.Close()
 
+	encoder := json.NewEncoder(outputFile)
+	encoder.SetIndent("", "  ") // For readable indentation
+	err = encoder.Encode(data)
+	if err != nil {
+		log.Fatalf("Error encoding JSON: %v", err)
+	}
+	log.Println("Process data successfully written to process_data.json")
 }
